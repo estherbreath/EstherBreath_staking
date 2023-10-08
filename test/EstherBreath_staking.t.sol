6e7d05c8a3219788cc33c0a1eae853a52a1a7b57
Esther Breath <estherojile248@gmail.com>
2023-10-08 19:32:53 +0100

    First commit

--- test/EstherBreath_staking.t.sol
+++ test/EstherBreath_staking.t.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-
 import {Test, console2} from "forge-std/Test.sol";
-
-
+import {EstherBreath_stakingTest} from "../src/Estherbreath_staking.sol";
 contract EstherBreath_stakingTest is Test {
     EstherBreath_staking public estherBreath_staking;
+
+     function setUp() public {
+       estherBreath_staking = new EstherBreath_staking();
+        
+    }
 }
